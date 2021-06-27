29bd280264650f250d95e9c5f0f6b1217ffa8be3
ELaneLyn <71429147+ELaneLyn@users.noreply.github.com>
2021-06-27 15:17:57 +0800

    Add files via upload

---
+++ musicList.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: 'Celebrity - LILAC',
+        artist: 'IU',
+        img: 'music-1',
+        src: 'music-1'
+    },
+    {
+        name: '나의 사춘기에게 - Red Diary Page.1',
+        artist: 'BOL4',
+        img: 'music-2',
+        src: 'music-2'
+    },
+    {
+        name: 'What the spring? - 봄이 좋냐??',
+        artist: '10cm',
+        img: 'music-3',
+        src: 'music-3'
+    },
+    {
+        name: '247 - An Ode',
+        artist: 'Seventeen',
+        img: 'music-4',
+        src: 'music-4'
+    },
+    {
+        name: '11:11',
+        artist: 'TAEYEON',
+        img: 'music-5',
+        src: 'music-5'
+    }
+]
